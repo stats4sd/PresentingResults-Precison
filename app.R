2388168c148e573c2ec304276603b0ee42c43570
Alex Thomson <alex.thomson@stats4sd.org>
2021-04-19 12:43:23 +0100

    improve reactivity

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(scales)
 library(doBy)
 library(DT)
+library(plyr)
 
 data <- data.frame(X = rnorm(100,100,25))
 
@@ -27,6 +28,12 @@ stat_aud <- function(mapping = NULL, data = NULL, geom = "area",
         params = list(xlim = xlim, n = n, ...))
 }
 
+get_hist <- function(p) {
+    d <- ggplot_build(p)$data[[1]]
+    data.frame(x = d$x, xmin = d$xmin, xmax = d$xmax, y = d$y)
+    return(d)
+}
+
 ui <- fluidPage(
     
     sidebarLayout(
@@ -38,7 +45,7 @@ ui <- fluidPage(
                                  label = "Select Standard Deviation (SD)",
                                  min = 1, max = 25, value = 10, step = 1),
                      actionButton(inputId = "refresh",
-                                  label = "Refresh")),
+                                  label = "New Sample")),
         mainPanel(
             titlePanel("Precision - Sample sizes and the Standard Deviation"),
             plotOutput("density"),
@@ -48,37 +55,101 @@ ui <- fluidPage(
 )
 
 server <- function(input,output){
+
+counter <- reactiveValues(n = 0)
+observeEvent(input$refresh, {counter$n <- counter$n + 1})    
     
-sampledata <- reactive({
-    set.seed(1)
+sampledata <- eventReactive(input$refresh, {
     data <- data.frame(X = rnorm(n = as.numeric(input$n), mean = 100, sd = as.numeric(input$SD)))
     return(data)
-})    
+})
+
+min_den <- eventReactive(input$refresh, {
+    data <- sampledata()
+    min_den <- 100 - input$SD
+    return(min_den)
+})
+
+max_den <- eventReactive(input$refresh, {
+    data <- sampledata()
+    min_den <- 100 + input$SD
+    return(min_den)
+})
+
+scale_min <- eventReactive(input$refresh, {
+    data <- sampledata()
     
-output$density <- renderPlot({
+    if(input$SD < 10){
+        scale_min <- plyr::round_any(min(data$X), 5, f = floor)
+    }else{
+        scale_min <- plyr::round_any(min(data$X), 10, f = floor)
+    }
+    return(scale_min)
+})
+
+scale_max <- eventReactive(input$refresh, {
+    data <- sampledata()
+    
+    if(input$SD < 10){
+        scale_max <- plyr::round_any(max(data$X), 5, f = ceiling)
+    }else{
+        scale_max <- plyr::round_any(max(data$X), 10, f = ceiling)
+    }
+    return(scale_max)
+})
+
+get_hist_dims <- eventReactive(input$refresh, {
+    data <- sampledata()
+    
+    p <- ggplot(data = data, aes(x = X))+
+    geom_histogram(binwidth = 1)
+
+    hist <- get_hist(p)
+    
+    return(hist)
+}
+)
+
+text_xmin <- eventReactive(input$refresh, {
+    data <- sampledata()
+    text_xmin <- 100 - (input$SD + (input$SD/8))
+    return(text_xmin)
+})
+
+text_xmax <- eventReactive(input$refresh, {
+    data <- sampledata()
+    text_xmax <- 100 + (input$SD + (input$SD/8))
+    return(text_xmax)
+})
+
+output$density <- 
+    renderPlot({
+        
+    if(counter$n==0){
+        ggplot(data, aes(x = X))+
+            theme_minimal()
+    }else{
+        
     data <- sampledata()
     
     density <- density(data$X)
     
-    min_den <- 100 - input$SD
-    max_den <- 100 + input$SD
-    
     ggplot(data = data, aes(x = X))+
         geom_density(colour = "palevioletred2")+
-        geom_vline(aes(xintercept = min_den),
+        geom_vline(aes(xintercept = min_den()),
                    linetype = "dashed")+
-        geom_vline(aes(xintercept = max_den),
+        geom_vline(aes(xintercept = max_den()),
                    linetype = "dashed")+
-        geom_text(aes(x = 100 - (input$SD + (input$SD/8)),
-                      y = max(density$y),
+        geom_text(aes(x = text_xmin(),
+                      y = max(density$y)*1.1,
                       label = "-1 SD"),
                       size = 6)+
-        geom_text(aes(x = 100 + (input$SD + (input$SD/8)),
-                      y = max(density$y),
+        geom_text(aes(x = text_xmax(),
+                      y = max(density$y*1.1),
                       label = "+1 SD"),
                   size = 6)+
         stat_aud(geom = "area",
-                 xlim = c(min_den, max_den),
+                 xlim = c(min_den(), max_den()),
                  alpha = 0.5,
                  fill = "palevioletred2")+
         labs(title = "Density Plot",
@@ -87,40 +158,39 @@ output$density <- renderPlot({
         theme_minimal()+
         theme(plot.title = element_text(size = 20),
               axis.title = element_text(size = 15),
-              axis.text = element_text(siz = 12))
+              axis.text = element_text(siz = 12))+
+        scale_x_continuous(limits = c(scale_min(), scale_max()))
+    }
 })
 
 output$histogram <- renderPlot({
+    
+    if(counter$n==0){
+        ggplot(data, aes(x = X))+
+            theme_minimal()
+    }else{
+        
     data <- sampledata()
-    
-    min_den <- 100 - input$SD
-    max_den <- 100 + input$SD
-    
-    get_hist <- function(p) {
-        d <- ggplot_build(p)$data[[1]]
-        data.frame(x = d$x, xmin = d$xmin, xmax = d$xmax, y = d$y)
-        return(d)
-    }
     
     p <- ggplot(data = data, aes(x = X))+
         geom_histogram(binwidth = 1)
     
-    hist <- get_hist(p)
+    hist <- get_hist_dims()
     
-    p+geom_vline(aes(xintercept = min_den),
+    p+geom_vline(aes(xintercept = min_den()),
                                     linetype = "dashed")+
-        geom_vline(aes(xintercept = max_den),
+        geom_vline(aes(xintercept = max_den()),
                    linetype = "dashed")+
-        geom_text(aes(x = 100 - (input$SD + (input$SD/8)),
-                      y = max(hist$y),
+        geom_text(aes(x = text_xmin(),
+                      y = max(hist$y)*1.1,
                       label = "-1 SD"),
                   size = 6)+
-        geom_text(aes(x = 100 + (input$SD + (input$SD/8)),
-                      y = max(hist$y),
+        geom_text(aes(x = text_xmax(),
+                      y = max(hist$y)*1.1,
                       label = "+1 SD"),
                   size = 6)+
-        annotate(geom = "rect", xmin = 100-input$SD,
-                xmax = 100+input$SD,
+        annotate(geom = "rect", xmin = min_den(),
+                xmax = max_den(),
                 ymin = 0,
                 ymax = max(hist$y),
                 fill = "palevioletred2",
@@ -132,7 +202,9 @@ output$histogram <- renderPlot({
         theme_minimal()+
         theme(plot.title = element_text(size = 20),
               axis.title = element_text(size = 15),
-              axis.text = element_text(siz = 12))
+              axis.text = element_text(siz = 12))+
+        scale_x_continuous(limits = c(scale_min(), scale_max()))
+    }
 })
     
 }
